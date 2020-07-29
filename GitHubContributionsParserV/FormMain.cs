53838ad0d843423bfa7423352e02a4552a87eba1
Dreamer <dreamerandprogrammer@gmail.com>
2020-07-29 21:02:34 +0300

    Add calculation commits per DayOfWeek

--- GitHubContributionsParserV/FormMain.cs
+++ GitHubContributionsParserV/FormMain.cs
@@ -26,11 +26,15 @@ namespace GitHubContributionsParserV
 			Data data = await ParseAsync();
 			//richTextBox1.Text = JsonConvert.SerializeObject(data, Formatting.Indented) + "\r\n";
 
+
+			richTextBox1.Text += "Year: \r\n";
+
 			foreach (YearData year in data.years)
 			{
 				richTextBox1.Text += $"{year.date.Year} - {year.counter}\r\n";
 			}
 
+			richTextBox1.Text += "Month: \r\n";
 
 			var groupedMonths = data.years[data.years.Count-1].calendar
 				.GroupBy(d => d.date.Month)
@@ -46,6 +50,27 @@ namespace GitHubContributionsParserV
 				}
 
 				richTextBox1.Text += $"{month[0].date.Month} - {counter}\r\n";
+			}
+
+			richTextBox1.Text += "DayOfWeek: \r\n";
+
+			var groupedDayOfWeek = data.years[data.years.Count - 1].calendar
+				.GroupBy(d => d.date.DayOfWeek)
+				.Select(grp => grp.ToList())
+				.ToList();
+
+			//((int)DateTime.Now.DayOfWeek + 6) % 7 + 1
+			groupedDayOfWeek.Sort((x, y) => (((int)x[0].date.DayOfWeek + 6) % 7 + 1).CompareTo(((int)y[0].date.DayOfWeek + 6) % 7 + 1));
+
+			foreach (List<DayData> dayOfWeek in groupedDayOfWeek)
+			{
+				int counter = 0;
+				foreach (DayData day in dayOfWeek)
+				{
+					counter += day.counter;
+				}
+
+				richTextBox1.Text += $"{dayOfWeek[0].date.DayOfWeek.ToString().Substring(0, 3)} - {counter}\r\n";
 			}
 		}
 
