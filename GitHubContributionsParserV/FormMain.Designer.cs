b813c3d22b718c23b24bfad63151442bda51ce14
Dreamer <dreamerandprogrammer@gmail.com>
2020-07-29 21:02:43 +0300

    Small changes

--- GitHubContributionsParserV/FormMain.Designer.cs
+++ GitHubContributionsParserV/FormMain.Designer.cs
@@ -62,6 +62,7 @@
 			// 
 			// richTextBox1
 			// 
+			this.richTextBox1.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.richTextBox1.Location = new System.Drawing.Point(415, 13);
 			this.richTextBox1.Name = "richTextBox1";
 			this.richTextBox1.Size = new System.Drawing.Size(585, 408);
