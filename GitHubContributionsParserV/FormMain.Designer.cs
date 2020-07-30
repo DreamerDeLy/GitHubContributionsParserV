88e69e995c9bac61c26e721233125a3723f85420
Dreamer <dreamerandprogrammer@gmail.com>
2020-07-30 23:17:06 +0300

    Small changes

--- GitHubContributionsParserV/FormMain.Designer.cs
+++ GitHubContributionsParserV/FormMain.Designer.cs
@@ -28,10 +28,8 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
 			this.btnParse = new System.Windows.Forms.Button();
 			this.tbUser = new System.Windows.Forms.TextBox();
-			this.formsPlot1 = new ScottPlot.FormsPlot();
 			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
 			this.SuspendLayout();
 			// 
@@ -53,19 +51,15 @@
 			this.tbUser.TabIndex = 1;
 			this.tbUser.Text = "DreamerDeLy";
 			// 
-			// formsPlot1
-			// 
-			this.formsPlot1.Location = new System.Drawing.Point(12, 71);
-			this.formsPlot1.Name = "formsPlot1";
-			this.formsPlot1.Size = new System.Drawing.Size(397, 350);
-			this.formsPlot1.TabIndex = 2;
-			// 
 			// richTextBox1
 			// 
+			this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
 			this.richTextBox1.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.richTextBox1.Location = new System.Drawing.Point(415, 13);
+			this.richTextBox1.Location = new System.Drawing.Point(12, 40);
 			this.richTextBox1.Name = "richTextBox1";
-			this.richTextBox1.Size = new System.Drawing.Size(585, 408);
+			this.richTextBox1.Size = new System.Drawing.Size(637, 432);
 			this.richTextBox1.TabIndex = 3;
 			this.richTextBox1.Text = "";
 			// 
@@ -73,9 +67,8 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1057, 450);
+			this.ClientSize = new System.Drawing.Size(661, 484);
 			this.Controls.Add(this.richTextBox1);
-			this.Controls.Add(this.formsPlot1);
 			this.Controls.Add(this.tbUser);
 			this.Controls.Add(this.btnParse);
 			this.Name = "FormMain";
@@ -89,7 +82,6 @@
 
 		private System.Windows.Forms.Button btnParse;
 		private System.Windows.Forms.TextBox tbUser;
-		private ScottPlot.FormsPlot formsPlot1;
 		private System.Windows.Forms.RichTextBox richTextBox1;
 	}
 }
