4ee86881eebcd69165fec3202adc190d38269b44
Dreamer <dreamerandprogrammer@gmail.com>
2020-08-26 22:20:25 +0300

    Interface changes

--- GitHubContributionsParserV/FormMain.Designer.cs
+++ GitHubContributionsParserV/FormMain.Designer.cs
@@ -32,17 +32,20 @@
 			this.btnParse = new System.Windows.Forms.Button();
 			this.tbUser = new System.Windows.Forms.TextBox();
 			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-			this.fpMonths = new ScottPlot.FormsPlot();
-			this.fpDayOfWeek = new ScottPlot.FormsPlot();
 			this.btnAnalyze = new System.Windows.Forms.Button();
 			this.cbYear = new System.Windows.Forms.ComboBox();
 			this.tbStartYear = new System.Windows.Forms.TextBox();
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.fpMonths = new ScottPlot.FormsPlot();
 			this.fpYears = new ScottPlot.FormsPlot();
+			this.fpDayOfWeek = new ScottPlot.FormsPlot();
+			this.fpDaysWihAndWithout = new ScottPlot.FormsPlot();
+			this.flowLayoutPanel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// btnParse
 			// 
-			this.btnParse.Location = new System.Drawing.Point(257, 11);
+			this.btnParse.Location = new System.Drawing.Point(161, 11);
 			this.btnParse.Name = "btnParse";
 			this.btnParse.Size = new System.Drawing.Size(75, 22);
 			this.btnParse.TabIndex = 0;
@@ -60,34 +63,19 @@
 			// 
 			// richTextBox1
 			// 
-			this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+			this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.richTextBox1.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.richTextBox1.Location = new System.Drawing.Point(12, 319);
+			this.richTextBox1.Location = new System.Drawing.Point(676, 39);
 			this.richTextBox1.Name = "richTextBox1";
-			this.richTextBox1.Size = new System.Drawing.Size(969, 287);
+			this.richTextBox1.Size = new System.Drawing.Size(300, 570);
 			this.richTextBox1.TabIndex = 3;
 			this.richTextBox1.Text = "";
-			// 
-			// fpMonths
-			// 
-			this.fpMonths.Location = new System.Drawing.Point(12, 40);
-			this.fpMonths.Name = "fpMonths";
-			this.fpMonths.Size = new System.Drawing.Size(320, 272);
-			this.fpMonths.TabIndex = 4;
-			// 
-			// fpDayOfWeek
-			// 
-			this.fpDayOfWeek.Location = new System.Drawing.Point(661, 40);
-			this.fpDayOfWeek.Name = "fpDayOfWeek";
-			this.fpDayOfWeek.Size = new System.Drawing.Size(320, 272);
-			this.fpDayOfWeek.TabIndex = 5;
 			// 
 			// btnAnalyze
 			// 
 			this.btnAnalyze.Enabled = false;
-			this.btnAnalyze.Location = new System.Drawing.Point(338, 11);
+			this.btnAnalyze.Location = new System.Drawing.Point(341, 11);
 			this.btnAnalyze.Name = "btnAnalyze";
 			this.btnAnalyze.Size = new System.Drawing.Size(75, 22);
 			this.btnAnalyze.TabIndex = 6;
@@ -99,7 +87,7 @@
 			// 
 			this.cbYear.Enabled = false;
 			this.cbYear.FormattingEnabled = true;
-			this.cbYear.Location = new System.Drawing.Point(419, 12);
+			this.cbYear.Location = new System.Drawing.Point(422, 12);
 			this.cbYear.Name = "cbYear";
 			this.cbYear.Size = new System.Drawing.Size(121, 21);
 			this.cbYear.TabIndex = 7;
@@ -107,36 +95,71 @@
 			// 
 			// tbStartYear
 			// 
-			this.tbStartYear.Location = new System.Drawing.Point(214, 12);
+			this.tbStartYear.Location = new System.Drawing.Point(118, 12);
 			this.tbStartYear.MaxLength = 4;
 			this.tbStartYear.Name = "tbStartYear";
 			this.tbStartYear.Size = new System.Drawing.Size(37, 20);
 			this.tbStartYear.TabIndex = 8;
 			this.tbStartYear.Text = "2017";
 			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.flowLayoutPanel1.AutoScroll = true;
+			this.flowLayoutPanel1.Controls.Add(this.fpMonths);
+			this.flowLayoutPanel1.Controls.Add(this.fpYears);
+			this.flowLayoutPanel1.Controls.Add(this.fpDayOfWeek);
+			this.flowLayoutPanel1.Controls.Add(this.fpDaysWihAndWithout);
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 39);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(658, 570);
+			this.flowLayoutPanel1.TabIndex = 10;
+			// 
+			// fpMonths
+			// 
+			this.fpMonths.Location = new System.Drawing.Point(3, 3);
+			this.fpMonths.Name = "fpMonths";
+			this.fpMonths.Size = new System.Drawing.Size(320, 272);
+			this.fpMonths.TabIndex = 5;
+			// 
 			// fpYears
 			// 
-			this.fpYears.Location = new System.Drawing.Point(335, 40);
+			this.fpYears.Location = new System.Drawing.Point(329, 3);
 			this.fpYears.Name = "fpYears";
 			this.fpYears.Size = new System.Drawing.Size(320, 272);
-			this.fpYears.TabIndex = 9;
+			this.fpYears.TabIndex = 10;
+			// 
+			// fpDayOfWeek
+			// 
+			this.fpDayOfWeek.Location = new System.Drawing.Point(3, 281);
+			this.fpDayOfWeek.Name = "fpDayOfWeek";
+			this.fpDayOfWeek.Size = new System.Drawing.Size(320, 272);
+			this.fpDayOfWeek.TabIndex = 11;
+			// 
+			// fpDaysWihAndWithout
+			// 
+			this.fpDaysWihAndWithout.Location = new System.Drawing.Point(329, 281);
+			this.fpDaysWihAndWithout.Name = "fpDaysWihAndWithout";
+			this.fpDaysWihAndWithout.Size = new System.Drawing.Size(320, 272);
+			this.fpDaysWihAndWithout.TabIndex = 12;
 			// 
 			// FormMain
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(993, 618);
-			this.Controls.Add(this.fpYears);
+			this.ClientSize = new System.Drawing.Size(988, 619);
+			this.Controls.Add(this.flowLayoutPanel1);
 			this.Controls.Add(this.tbStartYear);
 			this.Controls.Add(this.cbYear);
 			this.Controls.Add(this.btnAnalyze);
-			this.Controls.Add(this.fpDayOfWeek);
-			this.Controls.Add(this.fpMonths);
 			this.Controls.Add(this.richTextBox1);
 			this.Controls.Add(this.tbUser);
 			this.Controls.Add(this.btnParse);
 			this.Name = "FormMain";
 			this.Text = "ContributionsParserV";
+			this.flowLayoutPanel1.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -147,12 +170,14 @@
 		private System.Windows.Forms.Button btnParse;
 		private System.Windows.Forms.TextBox tbUser;
 		private System.Windows.Forms.RichTextBox richTextBox1;
-		private ScottPlot.FormsPlot fpMonths;
-		private ScottPlot.FormsPlot fpDayOfWeek;
 		private System.Windows.Forms.Button btnAnalyze;
 		private System.Windows.Forms.ComboBox cbYear;
 		private System.Windows.Forms.TextBox tbStartYear;
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+		private ScottPlot.FormsPlot fpMonths;
 		private ScottPlot.FormsPlot fpYears;
+		private ScottPlot.FormsPlot fpDayOfWeek;
+		private ScottPlot.FormsPlot fpDaysWihAndWithout;
 	}
 }
 
